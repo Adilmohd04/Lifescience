aaa82b7e65feabf5d9c4a9f41955fdd15d055403
Adilmohd04 <132822984+Adilmohd04@users.noreply.github.com>
2024-11-27 14:13:02 +0530

    Update Essential.tsx

--- app/components/Essential.tsx
+++ app/components/Essential.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import { GiMedicines } from "react-icons/gi";
@@ -28,42 +28,47 @@ const IconCard: React.FC<{ Icon: React.ElementType; title: string; description: 
 
 const Essential = () => {
   return (
-    <div className="h-screen bg-[#E2F5FB] flex flex-col justify-start items-center p-4 mb-16">
+    <div className="relative h-screen bg-[#E2F5FB] flex flex-col justify-start items-center p-4 mb-16">
       <h1 className="text-5xl sm:text-6xl lg:text-8xl text-center text-[#003569] font-bold">Essential Vitamins</h1>
-      
-      <section className="flex flex-col md:flex-row justify-center items-center gap-16 md:gap-24 mt-8">
 
+      <section className="flex flex-col md:flex-row justify-center items-center gap-16 md:gap-24 mt-8">
         <div className="text-center md:text-left">
           <p className="text-[#003569] text-lg sm:text-xl">Online Medical Supplies</p>
           <h4 className="text-2xl sm:text-3xl lg:text-4xl text-[#003569] font-bold mt-1">
-            Get Your Vitamins <br/> & Minerals
+            Get Your Vitamins <br /> & Minerals
           </h4>
           <Button text="Explore" className="mt-4" />
         </div>
 
         <div className="relative flex justify-center items-center sm:block">
-            <div className="absolute bg-[#FFE9B5] rounded-tl-[60px] rounded-br-[60px] w-32 h-32 sm:w-48 sm:h-48 md:w-72 md:h-72 lg:w-[330px] lg:h-[320px] z-0 right-4 top-12 translate-x-180 hidden lg:block" />
+          <div className="absolute bg-[#FFE9B5] rounded-tl-[60px] rounded-br-[60px] w-32 h-32 sm:w-48 sm:h-48 md:w-72 md:h-72 lg:w-[330px] lg:h-[320px] z-0 right-4 top-12 translate-x-180 hidden lg:block" />
+          <div className="relative z-10 -mt-16">
+            <Image
+              src="/probiotic.svg"
+              alt="Probiotic Image"
+              width={400}
+              height={400}
+              loading="lazy"
+              style={{ objectFit: "contain" }}
+              className="rounded-tl-[60px] rounded-br-[60px] hidden lg:block"
+            />
+          </div>
+        </div>
 
-            <div className="relative z-10 -mt-16">
-             <Image
-                src="/probiotic.svg"
-                alt="Probiotic Image"
-                width={400}
-                height={400}
-                loading="lazy"
-                style={{ objectFit: 'contain' }}
-                className="rounded-tl-[60px] rounded-br-[60px] hidden lg:block "
-                />
-
-            </div>
-            </div>
-
-        <div className="flex flex-col gap-9 -mt-20 md:mt-0">
+        <div className="flex flex-col gap-9 -mt-24 md:mt-0">
           <IconCard Icon={GiMedicines} title="Vitamins" description="Increased vitamins and minerals in your diet" />
           <IconCard Icon={FaWeight} title="Weight Loss" description="Find scientifically proven solutions for weight loss" />
           <IconCard Icon={MdOutlineEmojiFoodBeverage} title="Functional Foods" description="From protein powders to baby formula" />
         </div>
       </section>
+
+            <div className="hidden lg:flex flex-row gap-2 absolute left-80 top-3/4 transform -translate-x-1/2">
+        <div className="w-10 h-10 bg-[#A5B2C3] rounded-full"></div>
+        <div className="w-10 h-10 bg-[#A5B2C3] rounded-full"></div>
+      </div>
+
+
+
     </div>
   );
 };
